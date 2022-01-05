7613cd1f422f11bb9cc78e007d3df7bd2e0d5d71
David Alen <davidalen.dev@gmail.com>
2022-01-05 17:29:36 -0300

    add: run produce and consumer will consume

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const {Producer, Consumer} = require('kafka-node');
 const {client} = require('./client');
 
 const producer = new Producer(client);
-const consumer = new Consumer(client, [{topic: 'topic1'}]);
-
+const consumer = new Consumer(client, [{topic: 'topic2', offset: 0}]); // cursor = 2
+// ['topic1', 'topic2']
+// listen to topic1, listen to topic2
 consumer.on('message', function (message) {
     console.log("message", message);
 });
@@ -12,12 +13,15 @@ consumer.on('error', function (err) {
     console.error(err);
 })
 
+// 01, 02, 03, 04
+// 03, 04
+// {}, {}, {}, {}
 const produce = () => {
     const payloads = [
         {
             topic: 'topic1',
             messages: ['HEY'], // multi messages should be a array, single message can be just a string or a KeyedMessage instance
-            key: 'blabla123', // string or buffer, only needed when using keyed partitioner
+            key: 'blabla124', // string or buffer, only needed when using keyed partitioner
         }
     ]
 
